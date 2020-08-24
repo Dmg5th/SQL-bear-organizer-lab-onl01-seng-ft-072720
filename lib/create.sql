9fe6fba7fab7b4cb8c0fda6b150f300c49e7f28c
Daniel Gould <dmgould310@gmail.com>
2020-08-24 15:22:37 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,7 +1,11 @@
 CREATE TABLE bears (
 id INTEGER PRIMARY KEY,
                 name TEXT, 
-                age INTEGER
+                age INTEGER,
+                gender TEXT, 
+                color TEXT, 
+                temperament TEXT, 
+                
  
 );
 
