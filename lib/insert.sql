b00678698ea75ce9929d03fca74b417d167a80fc
Daniel Gould <dmgould310@gmail.com>
2020-08-24 15:35:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,6 +4,7 @@ INSERT INTO bears (name, age, color, temperament, alive) VALUES ('Tabitha', 12, 
 INSERT INTO bears (name, age, color, temperament, alive) VALUES ('Tabitha', 12, 'brown', 'sad', 0);
 INSERT INTO bears (name, age, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'brown', 'militant', 1);
 INSERT INTO bears (name, age, color, temperament, alive) VALUES ('Melissa', 10, 'brown', 'jubliant', 1);
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ('Grinch', 10, 'white', 'stubborn', 0);
 
 
 
