6c99490ca7864799224eb8634f0fdf3e12580140
Daniel Gould <dmgould310@gmail.com>
2020-08-24 15:32:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,6 @@
 INSERT INTO bears (name, age, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'brown', 'fun', 1);
-INSERT INTO bears (name, age, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'brown', 'fun', 1);
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ('Rowdy', 5, 'white', 'rowdy', 1);
+
 
 
 
